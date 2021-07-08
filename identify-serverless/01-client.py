818c2066deb1d648b8404bd469f3e0870c5fa458
Bob Kozdemba <bkozdemba@gmail.com>
2021-07-08 16:57:32 -0500

    initial code

---
+++ identify-serverless/01-client.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import requests
+
+time0 = (datetime(2021, 4, 22, 3, 40, 31, 994728),
+ None,
+ {'objects': [{'box': [207.0, 509.0, 299.0, 532.0],
+      'confidence': 0.8941678404808044,
+      'class': 'boats'},
+     {'box': [65.0, 326.0, 136.0, 403.0],
+      'confidence': 0.8731332421302795,
+      'class': 'boats'},
+     {'box': [1066.0, 109.0, 1142.0, 171.0],
+      'confidence': 0.8629531264305115,
+      'class': 'boats'},
+     {'box': [82.0, 602.0, 149.0, 671.0],
+      'confidence': 0.8608279228210449,
+      'class': 'boats'},
+     {'box': [298.0, 438.0, 383.0, 461.0],
+      'confidence': 0.8419623374938965,
+      'class': 'boats'},
+     {'box': [242.0, 0.0, 298.0, 19.0],
+      'confidence': 0.6364468932151794,
+      'class': 'boats'}]})
+#time1 = pipeline.snap()
+time1 = (datetime(2021, 4, 22, 3, 40, 32, 757943),
+ None,
+ {'objects': [{'box': [211.0, 508.0, 307.0, 532.0],
+      'confidence': 0.8908742070198059,
+      'class': 'boats'},
+     {'box': [1073.0, 113.0, 1147.0, 176.0],
+      'confidence': 0.8825204968452454,
+      'class': 'boats'},
+     {'box': [67.0, 325.0, 136.0, 405.0],
+      'confidence': 0.8686448931694031,
+      'class': 'boats'},
+     {'box': [81.0, 601.0, 151.0, 672.0],
+      'confidence': 0.8586115837097168,
+      'class': 'boats'},
+     {'box': [305.0, 438.0, 390.0, 462.0],
+      'confidence': 0.8368647694587708,
+      'class': 'boats'}]})
+
+IDENT_URL="http://192.168.0.3:8000"
+response = requests.post(IDENT_URL, json={"last": time0[2]}).json()
+print(f'response = {response}')
+
+#time0 = (time0[0], time0[1], response)
+#time0
+
+#
+# curl -X POST --data '{"last":{"objects": [{"box": [211.0, 508.0, 307.0, 532.0], "confidence": 0.8908742070198059, "class": "boats"}]}}' -H "Content-Type: application/json" http://192.168.0.3:8000
+#
---
+++ identify-serverless/identification.py
@@ -0,0 +1,125 @@
+import threading
+import numpy as np
+import scipy.spatial
+
+_ID_GEN_LOCK = threading.Lock()
+
+# Tracking parameters
+NEW_OBJ_THRESHOLD = 100  # pixels
+EXPIRE_THRESHOLD = 40  # frames
+
+
+def _db_generate():
+    return {'next_id': 0, 'missing': {}}
+
+
+def _id_generate(db):
+    _ID_GEN_LOCK.acquire(True)
+    next_id = db['next_id']
+    db['next_id'] += 1
+    _ID_GEN_LOCK.release()
+    return str(next_id)
+
+
+def _ensure_ids(meta, db):
+    """ Ensure all objects in frame have IDs. """
+    for item in meta['objects']:
+        if not item.get('id'):
+            item['id'] = _id_generate(db)
+
+
+def centroid_identify(time0, meta0, time1=None, meta1=None):
+    """ Identify tracked objects from previous frame to current frame. """
+    # initialize database if necessary
+    db = meta0.get('tracking')
+    if not db:
+        db = _db_generate()
+    _ensure_ids(meta0, db)
+
+    # return formatted frame data if no comparison was requested
+    if not meta1:
+        meta0['tracking'] = db
+        return meta0
+
+    # calculate centroids of all objects
+    centroids0 = [np.mean(np.array(item['box']).reshape((2, 2)), axis=0)
+                  for item in meta0['objects']]
+    ids0 = [item['id'] for item in meta0['objects']]
+    centroids1 = [np.mean(np.array(item['box']).reshape((2, 2)), axis=0)
+                  for item in meta1['objects']]
+
+    # add missing objects to old frame data
+    ids_missing = list(db['missing'].keys())
+    centroids_missing = [db['missing'][ident]['centroid'] for ident in ids_missing]
+    centroids0 += centroids_missing
+    ids0 += ids_missing
+                    
+    # placeholder in old frame for new objects
+    null = len(ids0)
+    ids0.append(None)
+
+    # calculate distance between all new and all old objects
+    distances = scipy.spatial.distance.cdist(centroids1, centroids0)
+    eligible = distances <= NEW_OBJ_THRESHOLD
+
+    # assume the shortest distance moved is the match
+    new2old = [distances[idx].argmin() for idx in range(distances.shape[0])]
+    new2old = [new2old[idx] if eligible[idx][new2old[idx]] else null for idx in range(distances.shape[0])]
+    ids1 = np.array(ids0)[new2old].tolist()
+                    
+    # assign ids to new objects
+    ids1 = [ident or _id_generate(db) for ident in ids1]
+    
+    # calculate speeds
+    time0 = time0 or 0
+    time1 = time1 or time0
+    dtime = time1 - time0
+    speeds1 = []
+    directions1 = []
+    for idx_new, idx_old in enumerate(new2old):
+        new_cent = centroids1[idx_new]
+        try:
+            old_cent = centroids0[idx_old]
+        except IndexError:
+            old_cent = new_cent
+
+        speed_vector = (np.array(new_cent) - np.array(old_cent)) / dtime
+        speed = np.linalg.norm(speed_vector)
+        direction = (np.arccos(np.dot([1, 0], speed_vector/speed)) \
+                    if speed > 0.000001 else 0.0) * 180 / np.pi
+        if speed_vector[1] > 0:
+            direction = 360 - direction
+
+        speeds1.append(speed)
+        directions1.append(direction)
+                    
+    # report found objects
+    for found in [ident for ident in ids_missing if ident in ids1]:
+        del db['missing'][found]
+                    
+    # report new missing objects
+    lost = [ident for ident in ids0 if ident and ident not in ids1 and ident not in ids_missing]
+    for ident in lost:
+        known_info = next(item for item in meta0['objects'] if item["id"] == ident)
+        last_loc = centroids0[ids0.index(ident)]
+        db['missing'][ident] = known_info
+        db['missing'][ident]['centroid'] = last_loc.tolist()
+        db['missing'][ident]['frames'] = 0
+                    
+    # bury expired objects
+    new_missing = {}
+    for ident, item in db['missing'].items():
+        item['frames'] += 1
+        if item['frames'] <= EXPIRE_THRESHOLD:
+            new_missing[ident] = item
+    db['missing'] = new_missing
+
+    # update metadata
+    for index in range(len(ids1)):
+        meta1['objects'][index]['id'] = ids1[index]
+        meta1['objects'][index]['centroid'] = centroids1[index].tolist()
+        meta1['objects'][index]['speed'] = speeds1[index]
+        meta1['objects'][index]['direction'] = directions1[index]
+    meta1['tracking'] = db
+                    
+    return meta1
